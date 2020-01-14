d51500a4bb4dcc890b4a62707d5a76dc9bed3773
Karl Taht <Karl.Taht@gmail.com>
2020-01-14 09:04:44 -0700

    New design

--- migrate.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
- ruby -r rubygems -e 'require "jekyll-import";
-    JekyllImport::Importers::Blogger.run({
-      "source"                => "../blog-01-14-2020.xml",
-      "no-blogger-info"       => false, # not to leave blogger-URL info (id and old URL) in the front matter
-      "replace-internal-link" => false, # replace internal links using the post_url liquid tag.
-    })'
